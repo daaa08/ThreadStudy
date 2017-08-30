718453572bd1c452a85192ed72810b193d288421
박다정 <daaa08@naver.com>
2017-08-30 15:24:11 +0900

    통신

--- app/src/main/java/com/da08/threadstudy/MainActivity.java
+++ app/src/main/java/com/da08/threadstudy/MainActivity.java
@@ -2,6 +2,7 @@ package com.da08.threadstudy;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -23,20 +24,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         urlEdit = (EditText)findViewById(R.id.url);
-        regexEdit = (EditText)findViewById(R.id.regx);
+        regexEdit = (EditText)findViewById(R.id.regex);
         resultTxt = (TextView)findViewById(R.id.result);
         crawlBtn = (Button)findViewById(R.id.crawl);
+        crawlBtn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         String url = urlEdit.getText().toString();
-        String regx = regexEdit.getText().toString();
-
-        new AsyncTask<String, String, String>() {
+        String regex = regexEdit.getText().toString();
+        AsyncTask a = new AsyncTask<String, String, String>() {
             @Override
             protected String doInBackground(String... strings) {
                 try {
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     URL url = new URL(strings[0].contains("http")?strings[0]:"http://"+strings[0]); //url
                     HttpURLConnection con = (HttpURLConnection) url.openConnection();
 //                    PrintWriter pw = new PrintWriter(con.getOutputStream());
@@ -48,28 +54,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //                    for(String s : protocol){
 //                        pw.println(s);
 //                    }
-                    Scanner scan = new Scanner(con.getInputStream());
-//                    scan.next("\\z");  // "\\z" 전체 문자 끝까지 다 가져옴
-                    while(scan.hasNext()){  // 내용물을 반복하여 다 가져오기
-                        scan.next();
+                    if( isCancelled()){
+                        return "Cancelled";
                     }
-
+                    Scanner scan = new Scanner(con.getInputStream());
+                    StringBuilder sb = new StringBuilder();
+//                    scan.next("\\z");  // "\\z" 전체 문자 끝까지 다 가져옴
+                    /*
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    for(String s : reader.lines()){
+                        Log.e("crawl", s);
+                    }
+                    */
+                    while(scan.hasNext()){
+                       sb.append(scan.next());
+                    }
+                    scan.close();
+                    return sb.toString();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-//                strings[1] //regex
                 return null;
             }
-
             @Override
             protected void onProgressUpdate(String... values) {
-                super.onProgressUpdate(values);
             }
-
             @Override
-            protected void onPostExecute(String s) {
-                super.onPostExecute(s);
+            protected void onPostExecute(@Nullable String s) {
+                resultTxt.setText(s);
             }
-        }.execute(url,regx);
+        }.execute(url,regex);
     }
 }
